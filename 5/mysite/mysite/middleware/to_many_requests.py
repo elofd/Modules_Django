a83a999b2e31933f2feb4bbe03f0066c01f405ae
elofd <fhnehbec@gmail.com>
2023-01-15 18:05:08 +0500

    module5

---
+++ 5/mysite/mysite/middleware/to_many_requests.py
@@ -0,0 +1,12 @@
+# from django.utils.decorators import method_decorator
+# from django_ratelimit.decorators import ratelimit
+#
+#
+# @method_decorator(ratelimit(key='ip', rate='5/m', method='GET'), name='__call__')
+# class ToManyRequests:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
+#
+#     def __call__(self, request):
+#         response = self.get_response(request)
+#         return response
